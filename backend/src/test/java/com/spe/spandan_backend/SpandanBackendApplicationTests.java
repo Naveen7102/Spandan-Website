b016ec772087db8d09c2b5518fd5f6e36d6a6347
kanig <ssvnaveen@gmail.com>
2023-04-12 11:28:56 +0530

    Base Code

---
+++ backend/src/test/java/com/spe/spandan_backend/SpandanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spe.spandan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpandanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
