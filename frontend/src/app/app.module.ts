2f1fd84923c7c3b265eb0ee1632d1ddd9b4cf34d
kanig <ssvnaveen@gmail.com>
2023-04-15 02:19:46 +0530

    created files for login and signup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AppComponent } from './app.component';
 import AlertModule from "ngx-bootstrap";
 import { SportsListComponent } from './components/sports-list/sports-list.component';
 import { FixturesComponent } from './components/fixtures/fixtures.component';
-import { CreateOrJointTeamComponent } from './components/create-or-joint-team/create-or-joint-team.component'
+import { CreateOrJointTeamComponent } from './components/create-or-joint-team/create-or-joint-team.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     SportsListComponent,
     FixturesComponent,
-    CreateOrJointTeamComponent
+    CreateOrJointTeamComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
