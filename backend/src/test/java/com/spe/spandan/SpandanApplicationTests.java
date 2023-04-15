df0b301bd58aadf59b43f15867d9c79f21d86f04
manoharsuggula <manoharsuggula@gmail.com>
2023-04-15 23:44:12 +0530

    Added Models and Repositories

---
+++ backend/src/test/java/com/spe/spandan/SpandanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spe.spandan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpandanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
